b7d52cc3ee66c9d902651c4c77ed50f46441b7b6
gabo995 <gabelittleorange@gmail.com>
2021-01-22 18:07:35 -0600

    Dynamic listing

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -3,11 +3,14 @@ import { FaXbox, FaWindows, FaPlaystation } from "react-icons/fa";
 import { IoThumbsUpSharp, IoThumbsDownSharp } from "react-icons/io5";
 import { SiNintendoswitch } from "react-icons/si";
 import { months } from "../data/months";
+import { platforms } from "../data/platforms";
 import { Button } from "@material-ui/core";
 import ReactPlayer from "react-player";
 import "../css/GamesList.css";
+import { GamepadRounded } from "@material-ui/icons";
 
 function GamesList({ games }) {
+  console.log(games);
   const [currentHover, setCurrentHover] = useState(-1);
   const [playVideo, setPlayVideo] = useState(false);
 
@@ -23,222 +26,134 @@ function GamesList({ games }) {
   return (
     <div className="games_list">
       <div className="games_list_container">
-        <div className="game_card">
-          <div className="game_card_image_container">
-            <img src={games[0].background_image} alt="" />
-          </div>
-          <div className="game_card_info">
-            <div className="game_card_platforms">
-              <div className="game_card_platform_image">
-                <FaXbox />
-              </div>
-              <div className="game_card_platform_image">
-                <FaWindows />{" "}
-              </div>
-              <div className="game_card_platform_image">
-                <FaPlaystation />
-              </div>
-            </div>
-            <div className="game_card_title_wrapper">
-              <h3>{games[0].name}</h3>
-            </div>
-          </div>
-        </div>
-        <div style={{ position: "relative" }}>
-          <div
-            className={`game_card ${
-              currentHover === 1 ? "show_more_container" : ""
-            }`}
-            onMouseEnter={() => onHoverHandler(1)}
-            onMouseLeave={() => onHoverHandler(-1)}
-          >
-            <div
-              className="game_card_image_container"
-              onClick={() => {
-                setPlayVideo(!playVideo);
-              }}
-            >
-              <div className="video_player" style={{ position: "relative" }}>
-                <ReactPlayer
-                  style={{
-                    position: "absolute",
-                    transition: "500ms",
-                    borderTopLeftRadius: "1em",
-                    borderTopRightRadius: "1em",
-                  }}
-                  width="100%"
-                  height="auto"
-                  loop
-                  muted
-                  fallback
-                  playing={playVideo}
-                  url={`${
-                    currentHover === 1 && playVideo && games[1].clip.clip
-                  }`}
-                />
-              </div>
-              <img src={games[1].background_image} alt="" />
-            </div>
-            <div className="game_card_info">
-              <div className="game_card_platforms">
-                <div className="game_card_platform_image">
-                  <FaXbox />
-                </div>
-                <div className="game_card_platform_image">
-                  <FaWindows />{" "}
-                </div>
-                <div className="game_card_platform_image">
-                  <FaPlaystation />
-                </div>
-              </div>
-              <div className="game_card_title_wrapper">
-                <h3>{games[1].name}</h3>
-              </div>
-              <br></br>
-              <div className="game_card_more_container">
+        {games.map((game, index) => {
+          console.log("game", game);
+          return (
+            <div style={{ position: "relative" }}>
+              <div
+                className={`game_card ${
+                  currentHover === index ? "show_more_container" : ""
+                }`}
+                onMouseEnter={() => onHoverHandler(index)}
+                onMouseLeave={() => onHoverHandler(-1)}
+              >
                 <div
-                  className={`game_card_more_section ${
-                    currentHover === 1 ? "show" : ""
-                  }`}
+                  className="game_card_image_container"
+                  onClick={() => {
+                    setPlayVideo(!playVideo);
+                  }}
                 >
-                  <div className="game_card_more_about_table">
-                    <div className="game_card_table_row_container">
-                      <div className="game_card_table_row">
-                        <div className="row_title">Genres:</div>
-                        {games[1].genres.map((genre, index) => (
-                          <div className="row_element" key={index}>
-                            {genre.name}
-                            {index !== games[1].genres.length - 1 && ","}
-                          </div>
-                        ))}
-                      </div>
-                      <div className="game_card_table_row">
-                        <div className="row_title">Ratings:</div>
+                  <div
+                    className="video_player"
+                    style={{ position: "relative" }}
+                  >
+                    <ReactPlayer
+                      style={{
+                        position: "absolute",
+                        transition: "500ms",
+                        borderTopLeftRadius: "1em",
+                        borderTopRightRadius: "1em",
+                      }}
+                      width="100%"
+                      height="auto"
+                      loop
+                      muted
+                      fallback
+                      playing={playVideo}
+                      url={`${
+                        currentHover === index && playVideo && game.clip.clip
+                      }`}
+                    />
+                  </div>
+                  <img src={game.background_image} alt="" />
+                </div>
+                <div className="game_card_info">
+                  <div className="game_card_platforms">
+                    {game.parent_platforms.map((platform) => {
+                      let foundPlatform = platforms.find(
+                        (element) =>
+                          element === platform.platform.name.toLowerCase()
+                      );
 
-                        <div className="row_element">
-                          <div className="ratings_positive">
-                            <div className="positive_icon">
-                              <IoThumbsUpSharp />
-                            </div>
-                            <div className="positive_percent">
-                              {Math.round(
-                                games[1].ratings[0].percent +
-                                  games[1].ratings[1].percent
-                              )}
-                              %
-                            </div>
+                      return (
+                        foundPlatform && (
+                          <div className="game_card_platform_image">
+                            {foundPlatform === "xbox" && <FaXbox />}
+                            {foundPlatform === "pc" && <FaWindows />}
+                            {foundPlatform === "playstation" && (
+                              <FaPlaystation />
+                            )}
+                            {foundPlatform === "nintendo" && (
+                              <SiNintendoswitch />
+                            )}
                           </div>
-                          <div className="ratings_negative">
-                            <div className="negative_icon">
-                              <IoThumbsDownSharp />
-                            </div>
-                            <div className="negative_percent">
-                              {Math.round(
-                                games[1].ratings[2].percent +
-                                  games[1].ratings[3].percent
-                              )}
-                              %
-                            </div>
+                        )
+                      );
+                    })}
+                  </div>
+                  <div className="game_card_title_wrapper">
+                    <h3>{game.name}</h3>
+                  </div>
+                  <br></br>
+                  <div className="game_card_more_container">
+                    <div
+                      className={`game_card_more_section ${
+                        currentHover === index ? "show" : ""
+                      }`}
+                    >
+                      <div className="game_card_more_about_table">
+                        <div className="game_card_table_row_container">
+                          <div className="game_card_table_row">
+                            <div className="row_title">Genres:</div>
+                            {game.genres.map((genre, index) => (
+                              <div className="row_element" key={index}>
+                                {genre.name}
+                                {index !== game.genres.length - 1 && ","}
+                              </div>
+                            ))}
                           </div>
-                        </div>
-                      </div>
+                          {game.metacritic && (
+                            <div className="game_card_table_row">
+                              <div className="row_title">Metacritic:</div>
+                              <div className="row_element">
+                                {game.metacritic} %
+                              </div>
+                            </div>
+                          )}
 
-                      <div className="game_card_table_row">
-                        <div className="row_title">Release Date:</div>
-                        <div className="row_element">
-                          {
-                            months[
-                              parseInt(games[1].released.split("-")[1]) - 1
-                            ]
-                          }
-                        </div>
-                        <div className="row_element">
-                          {games[1].released.split("-")[2]}
-                        </div>
-                        <div className="row_element">
-                          ,{games[1].released.split("-")[0]}
+                          <div className="game_card_table_row">
+                            <div className="row_title">Release Date:</div>
+                            <div className="row_element">
+                              {
+                                months[
+                                  parseInt(game.released.split("-")[1]) - 1
+                                ]
+                              }
+                            </div>
+                            <div className="row_element">
+                              {game.released.split("-")[2]}
+                            </div>
+                            <div className="row_element">
+                              ,{game.released.split("-")[0]}
+                            </div>
+                          </div>
                         </div>
                       </div>
+                      <Button
+                        variant="contained"
+                        fullWidth
+                        color="primary"
+                        style={{ color: "white", marginTop: "0.5em" }}
+                      >
+                        See More
+                      </Button>
                     </div>
                   </div>
-                  <Button
-                    variant="contained"
-                    fullWidth
-                    color="primary"
-                    style={{ color: "white", marginTop: "0.5em" }}
-                  >
-                    See More
-                  </Button>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div className="game_card">
-          <div className="game_card_image_container">
-            <img src={games[2].background_image} alt="" />
-          </div>
-          <div className="game_card_info">
-            <div className="game_card_platforms">
-              <div className="game_card_platform_image">
-                <FaXbox />
-              </div>
-              <div className="game_card_platform_image">
-                <FaWindows />{" "}
-              </div>
-              <div className="game_card_platform_image">
-                <FaPlaystation />
-              </div>
-            </div>
-            <div className="game_card_title_wrapper">
-              <h3>{games[2].name}</h3>
-            </div>
-          </div>
-        </div>
-
-        <div className="game_card">
-          <div className="game_card_image_container">
-            <img src={games[3].background_image} alt="" />
-          </div>
-          <div className="game_card_info">
-            <div className="game_card_platforms">
-              <div className="game_card_platform_image">
-                <FaXbox />
-              </div>
-              <div className="game_card_platform_image">
-                <FaWindows />{" "}
-              </div>
-              <div className="game_card_platform_image">
-                <FaPlaystation />
-              </div>
-            </div>
-            <div className="game_card_title_wrapper">
-              <h3>{games[3].name}</h3>
-            </div>
-          </div>
-        </div>
-        <div className="game_card">
-          <div className="game_card_image_container">
-            <img src={games[4].background_image} alt="" />
-          </div>
-          <div className="game_card_info">
-            <div className="game_card_platforms">
-              <div className="game_card_platform_image">
-                <FaXbox />
-              </div>
-              <div className="game_card_platform_image">
-                <FaWindows />{" "}
-              </div>
-              <div className="game_card_platform_image">
-                <FaPlaystation />
-              </div>
-            </div>
-            <div className="game_card_title_wrapper">
-              <h3>{games[4].name}</h3>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
